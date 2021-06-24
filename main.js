9226c9660365d7415c993afd69beb22bf04778ee
maxbendas <maxbendas86@gmail.com>
2021-06-24 15:51:27 +0300

    day 2 with DZ

--- main.js
+++ main.js
@@ -16,31 +16,99 @@ const keys = {
 const settings = {
     start: false,
     score: 0,
-    speed:3
+    speed: 3,
+    traffic:3
 }
 
-const startGame = ()=>{
-    start.classList.add('hide');
-    settings.start=true
-    gameArea.append(car)
-    requestAnimationFrame(playGame)
+const moveRoad = ()=>{
+    let lines = document.querySelectorAll('.line')
+    lines.forEach((line)=>{
+        line.y+=settings.speed
+        line.style.top = line.y+'px'
+
+        if (line.y>=document.documentElement.clientHeight){
+            line.y = -100
+        }
+    })
 }
 
-const playGame = ()=>{
+const moveEnemy = ()=>{
+    let enemy = document.querySelectorAll('.enemy')
+    enemy.forEach((item)=>{
+        item.y+=settings.speed/2
+        item.style.top = item.y+'px'
+
+        if (item.y>=document.documentElement.clientHeight){
+            item.y = -100*settings.traffic
+            item.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50)) + 'px'
+        }
+    })
+}
+
+const playGame = () => {
+    moveEnemy()
+    moveRoad()
     console.log('play game')
-    if(settings.start){
+    if (settings.start) {
+        if (keys.ArrowLeft && settings.x>0) {
+            settings.x -= settings.speed
+        }
+        if (keys.ArrowRight&& settings.x<(gameArea.offsetWidth-car.offsetWidth)) {
+            settings.x += settings.speed
+        }
+        if (keys.ArrowUp && settings.y>0) {
+            settings.y -= settings.speed
+        }
+        if (keys.ArrowDown && settings.y<(gameArea.offsetHeight-car.offsetHeight)) {
+            settings.y += settings.speed
+        }
+
+        car.style.left = settings.x + 'px'
+        car.style.top = settings.y + 'px'
         requestAnimationFrame(playGame)
     }
 }
 
-const startRun = (e)=>{
+const getQuantityElements = (heightElement) =>{
+    return document.documentElement.clientHeight/heightElement+1
+}
+
+const startGame = () => {
+    start.classList.add('hide');
+
+    for(let i=0; i<getQuantityElements(100); i++){
+        const line = document.createElement('div')
+        line.classList.add('line')
+        line.style.top = (i*100)+'px'
+        line.y = i*100
+        gameArea.append(line)
+    }
+
+    for(let i=0; i<getQuantityElements(100 * settings.traffic); i++){
+        const enemy = document.createElement('div')
+        enemy.classList.add('enemy')
+        enemy.y = -100 * settings.traffic * (i+1)
+        enemy.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50)) + 'px'
+        enemy.style.top = enemy.y +'px'
+        enemy.style.background = 'transparent url(\'./image/enemy.png\') center / cover no-repeat'
+        gameArea.append(enemy)
+    }
+
+    settings.start = true
+    gameArea.appendChild(car)
+    settings.x = car.offsetLeft
+    settings.y = car.offsetTop
+    requestAnimationFrame(playGame)
+}
+
+const startRun = (e) => {
     e.preventDefault()
     keys[e.key] = true
 }
 
-const stopRun = (e)=>{
+const stopRun = (e) => {
     e.preventDefault()
-    keys[e.key] = true
+    keys[e.key] = false
 }
 
 start.addEventListener('click', startGame);

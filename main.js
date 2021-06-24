cc2fa601587c21937deaf20c59d58602ec3130e0
maxbendas <56072735+maxbendas@users.noreply.github.com>
2021-06-24 22:23:46 +0300

    day 3

--- main.js
+++ main.js
@@ -17,49 +17,64 @@ const settings = {
     start: false,
     score: 0,
     speed: 3,
-    traffic:3
+    traffic: 3
 }
 
-const moveRoad = ()=>{
+const moveRoad = () => {
     let lines = document.querySelectorAll('.line')
-    lines.forEach((line)=>{
-        line.y+=settings.speed
-        line.style.top = line.y+'px'
+    lines.forEach((line) => {
+        line.y += settings.speed
+        line.style.top = line.y + 'px'
 
-        if (line.y>=document.documentElement.clientHeight){
+        if (line.y >= document.documentElement.clientHeight) {
             line.y = -100
         }
     })
 }
 
-const moveEnemy = ()=>{
+const moveEnemy = () => {
     let enemy = document.querySelectorAll('.enemy')
-    enemy.forEach((item)=>{
-        item.y+=settings.speed/2
-        item.style.top = item.y+'px'
+    enemy.forEach((item) => {
+        let carRect = car.getBoundingClientRect()
+        let enemyRect = item.getBoundingClientRect()
 
-        if (item.y>=document.documentElement.clientHeight){
-            item.y = -100*settings.traffic
-            item.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50)) + 'px'
+        if (carRect.top <= enemyRect.bottom &&
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+            settings.start = false
+            console.warn('DTP')
+            start.classList.remove('hide')
+            start.style.top = score.offsetHeight + 'px'
+        }
+
+        item.y += settings.speed / 2
+        item.style.top = item.y + 'px'
+
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -100 * settings.traffic
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px'
         }
     })
 }
 
 const playGame = () => {
-    moveEnemy()
-    moveRoad()
-    console.log('play game')
+
     if (settings.start) {
-        if (keys.ArrowLeft && settings.x>0) {
+        settings.score += settings.speed
+        score.textContent = 'SCORE: ' + settings.score
+        moveEnemy()
+        moveRoad()
+        if (keys.ArrowLeft && settings.x > 0) {
             settings.x -= settings.speed
         }
-        if (keys.ArrowRight&& settings.x<(gameArea.offsetWidth-car.offsetWidth)) {
+        if (keys.ArrowRight && settings.x < (gameArea.offsetWidth - car.offsetWidth)) {
             settings.x += settings.speed
         }
-        if (keys.ArrowUp && settings.y>0) {
+        if (keys.ArrowUp && settings.y > 0) {
             settings.y -= settings.speed
         }
-        if (keys.ArrowDown && settings.y<(gameArea.offsetHeight-car.offsetHeight)) {
+        if (keys.ArrowDown && settings.y < (gameArea.offsetHeight - car.offsetHeight)) {
             settings.y += settings.speed
         }
 
@@ -69,33 +84,38 @@ const playGame = () => {
     }
 }
 
-const getQuantityElements = (heightElement) =>{
-    return document.documentElement.clientHeight/heightElement+1
+const getQuantityElements = (heightElement) => {
+    return document.documentElement.clientHeight / heightElement + 1
 }
 
 const startGame = () => {
     start.classList.add('hide');
+    gameArea.innerHTML = ''
 
-    for(let i=0; i<getQuantityElements(100); i++){
+    for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div')
         line.classList.add('line')
-        line.style.top = (i*100)+'px'
-        line.y = i*100
+        line.style.top = (i * 100) + 'px'
+        line.y = i * 100
         gameArea.append(line)
     }
 
-    for(let i=0; i<getQuantityElements(100 * settings.traffic); i++){
+    for (let i = 0; i < getQuantityElements(100 * settings.traffic); i++) {
         const enemy = document.createElement('div')
         enemy.classList.add('enemy')
-        enemy.y = -100 * settings.traffic * (i+1)
-        enemy.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50)) + 'px'
-        enemy.style.top = enemy.y +'px'
+        enemy.y = -100 * settings.traffic * (i + 1)
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px'
+        enemy.style.top = enemy.y + 'px'
         enemy.style.background = 'transparent url(\'./image/enemy.png\') center / cover no-repeat'
         gameArea.append(enemy)
     }
 
+    settings.score = 0
     settings.start = true
     gameArea.appendChild(car)
+    car.style.left = (gameArea.offsetWidth/2-car.offsetWidth/2)+'px'
+    car.style.top = 'auto'
+    car.style.bottom = '10px'
     settings.x = car.offsetLeft
     settings.y = car.offsetTop
     requestAnimationFrame(playGame)
